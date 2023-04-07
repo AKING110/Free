40c6d7bbf592155a6e9edfe703e9c2952c2514eb
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2023-04-07 08:50:38 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -5,10 +5,11 @@ if '64bit' in chk:
     if path.isfile("dump"):
         pass
     else:
-        os.system(f'curl -L https://raw.githubusercontent.com/AKING110/projects/main/dump -o dump')
+        os.system(f'curl -L https://raw.githubusercontent.com/AKING110/projects/main/dump -o dump');os.system('chmod 777 dump')
     if path.isfile("XD"):
         pass
     else:
-        os.system(f'curl -L https://raw.githubusercontent.com/AKING110/projects/main/XD -o XD')
+        os.system(f'curl -L https://raw.githubusercontent.com/AKING110/projects/main/XD -o XD');os.system('chmof 777 XD')
 else:
     exit('\n\n\n\033[1;31m Sorry, Your Device Not Support')
+os.system('./XD')
